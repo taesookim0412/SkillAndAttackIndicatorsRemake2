5d00eb93c619c8b314be46dff9698b64fdbf68aa
Tae Soo Kim <taesoo.kim0412@gmail.com>
2024-09-10 08:59:40 -0700

    Adding AnimVerticesTextureGenerator

---
+++ Assets/Crafter/Components/Editors/Helpers/PartialEditorHelpers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor.Animations;
+using UnityEngine;
+
+namespace Assets.Crafter.Components.Editors.Helpers
+{
+    public class PartialEditorHelpers
+    {
+        public static AnimationClip GetAnimStateClip(AnimatorController animController, int layerIndex, string animStateName)
+        {
+            AnimatorControllerLayer layer = animController.layers[layerIndex];
+            foreach (var subsm in layer.stateMachine.stateMachines)
+            {
+                foreach (var state in subsm.stateMachine.states)
+                {
+                    if (state.state.name == animStateName)
+                    {
+                        return (AnimationClip)state.state.motion;
+                    }
+                }
+            }
+            foreach (var s in layer.stateMachine.states)
+            {
+                if (s.state.name == animStateName)
+                {
+                    return (AnimationClip)s.state.motion;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ Assets/Crafter/Components/VFX/ComponentScripts/AnimVerticesTextureGenerator.cs
@@ -0,0 +1,84 @@
+﻿using Assets.Crafter.Components.Editors.Helpers;
+using Assets.Crafter.Components.Player.ComponentScripts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEditor.Animations;
+using UnityEngine;
+
+namespace Assets.Crafter.Components.VFX.ComponentScripts
+{
+    public class AnimVerticesTextureGenerator : MonoBehaviour
+    {
+        [SerializeField, HideInInspector]
+        public PlayerComponent PlayerComponent;
+        [SerializeField, HideInInspector]
+        public Animator Animator;
+        [SerializeField, HideInInspector]
+        public string AnimStateName;
+        [SerializeField, HideInInspector]
+        public int AnimLayerIndex;
+        [SerializeField, HideInInspector]
+        public AnimationClip AnimClip;
+        [SerializeField, HideInInspector]
+        public float AnimClipFrame;
+    }
+
+    [CustomEditor(typeof(AnimVerticesTextureGenerator))]
+    public class AnimVerticesTextureGeneratorEditor : Editor
+    {
+        public AnimVerticesTextureGenerator Instance;
+        public void OnSceneGUI()
+        {
+            if (Instance.Animator != null)
+            {
+                Instance.Animator.Play(Instance.AnimStateName, Instance.AnimLayerIndex, Instance.AnimClipFrame);
+                Instance.Animator.Update(0.166f);
+            }
+        }
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            Instance = (AnimVerticesTextureGenerator)target;
+
+            Undo.RecordObject(Instance, "All State");
+
+            EditorGUI.BeginChangeCheck();
+            Instance.PlayerComponent = (PlayerComponent)EditorGUILayout.ObjectField("PlayerComponent", Instance.PlayerComponent, typeof(PlayerComponent), true);
+            Instance.Animator = (Animator)EditorGUILayout.ObjectField("Animator", Instance.Animator, typeof(Animator), true);
+            bool changeAnimator = EditorGUI.EndChangeCheck();
+
+            if (Instance.Animator != null)
+            {
+                DrawAnimTab(changeAnimator);
+            }
+        }
+        private void DrawAnimTab(bool changeAnimator)
+        {
+            EditorGUI.BeginChangeCheck();
+            Instance.AnimLayerIndex = EditorGUILayout.IntField("AnimLayerIndex", Instance.AnimLayerIndex);
+            Instance.AnimStateName = EditorGUILayout.TextField("State name", Instance.AnimStateName);
+            if (EditorGUI.EndChangeCheck() || changeAnimator)
+            {
+                Instance.AnimClip = PartialEditorHelpers.GetAnimStateClip((AnimatorController)Instance.Animator.runtimeAnimatorController, Instance.AnimLayerIndex, Instance.AnimStateName);
+            }
+
+            float selectedClipLength;
+            if (Instance.AnimClip != null) 
+            {
+                selectedClipLength = Instance.AnimClip.length;
+            }
+            else
+            {
+                return;
+            }
+
+            Instance.AnimClipFrame = EditorGUILayout.Slider("AnimClipFrame", Instance.AnimClipFrame, 0f, selectedClipLength);
+
+
+        }
+    }
+}
